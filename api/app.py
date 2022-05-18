d0f610b1696901fa4d28a2aed84d06ca2b4bab20
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2022-05-18 23:54:39 +0700

    Adding app.py in api

---
+++ api/app.py
@@ -0,0 +1,11 @@
+# This is the entry point for the command line script.
+
+# The main function.
+def main():
+    print("Hello World!")
+
+# Dont bother below script.
+if __name__ == "__main__":
+    main()
+
+# Stop right here. You dont need to exceed this line.
